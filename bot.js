0407d11b839745fdfbd88646e596b78e5330405f
adsumusx <52834544+adsumusx@users.noreply.github.com>
2019-07-26 13:46:07 -0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+
+client.on("ready", () => {
+    console.log(`Bot foi iniciado`);
+    client.user.setGame(`XanFrango`);
+})
+
+client.on("message", async message => {
+
+    if (message.author.bot) return;
+    if (message.channel.type === "dm") return;
+    if (!message.content.startsWith(config.prefix)) return;
+
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const comando = args.shift().toLowerCase();
+
+    // coamdno ping
+    if (comando === "ping") {
+        const m = await message.channel.send("Ping?");
+        m.edit(`Pong! A Latência é ${m.createdTimestamp - message.createdTimestamp}ms. A Latencia da API é ${Math.round(client.ping)}ms`);
+    }
+});
+
+client.login(config.token);
