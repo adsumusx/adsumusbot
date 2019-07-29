51356a99b69fe6cd16d7e122cda89176399b6b44
adsumusx <adsumusx@gmail.com>
2019-07-28 23:54:44 -0300

    adicionando o firebase e os comandos xp e nivel.

--- bot.js
+++ bot.js
@@ -1,26 +1,87 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+const firebase = require("firebase");
 //ADICIONE UM 8 NO ARQUIVO CONFIG.JSON NO FIM DO TOKEN PARA FUNCIONAR
+
+//Configurações do firebase-
+var configBase = {
+    apiKey: "AIzaSyA0gYRZa9J3dj1pcoJq2posSOSs6WTwzb4",
+    authDomain: "adsumus-bot.firebaseapp.com",
+    databaseURL: "https://adsumus-bot.firebaseio.com",
+    projectId: "adsumus-bot",
+    storageBucket: "adsumus-bot.appspot.com",
+    messagingSenderId: "1073172880535",
+    appId: "1:1073172880535:web:7e39fc4c96e368de"
+};
+
+firebase.initializeApp(configBase);
+const database = firebase.database();
+
+global.prefix = '';
+global.xp = '';
+global.proxNivel = '';
+global.ouro = '';
+global.nivel = '';
+
 client.on("ready", () => {
     console.log(`Bot foi iniciado`);
     client.user.setGame(`XanFrango`);
 })
 
 client.on("message", async message => {
+    const idCliente = message.author.id;
+    const adsumus = config.adsumus == idCliente;
+    const idBot = config.id_bot == idCliente
+    if(!idBot){
+        database.ref(`servidores/niveis/${message.guild.id}/${message.author.id}`)
+        .once('value').then(async function (data) {
+            if (data.val() == null) {
+                database.ref(`servidores/niveis/${message.guild.id}/${message.author.id}`)
+                    .set({
+                        xp: 0,
+                        nivel: 1
+                    });
+            }
+            else {
+                xp = data.val().xp + addPontos;
+                nivel = data.val().nivel;
+                proxNivel = data.val().nivel * 500;
+                database.ref(`servidores/niveis/${message.guild.id}/${message.author.id}`)
+                    .update({
+                        xp: xp
+                    })
+                if (proxNivel <= xp) {
+                    proxNivel = data.val().nivel + 1
+                    database.ref(`servidores/niveis/${message.guild.id}/${message.author.id}`)
+                        .update({
+                            nivel: proxNivel
+                        })
+                    await message.channel.send(` ${message.author.username} subiu de nivel!`)
+                }
+            }
+        });
+    let addPontos = Math.floor(Math.random() * 7) + 8;
+    }
     if (message.author.bot) return;
     if (message.channel.type === "dm") return;
     if (!message.content.startsWith(config.prefix)) return;
 
+    //Nivel
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const comando = args.shift().toLowerCase();
-    const idCliente = message.author.id;
-    const adsumus = config.adsumus == idCliente;
 
     if (comando === "ping") {
         let m = await message.channel.send("Ping?");
         m.edit(`Pong! A Latência é ${m.createdTimestamp - message.createdTimestamp}ms. A Latencia da API é ${Math.round(client.ping)}ms`);
     }
+    if (comando === "nivel" || comando === "level") {
+        message.channel.send(` Seu nivel é -> ${nivel}`);
+    }
+    if (comando === "xp" || comando === "experiencia") {
+        message.channel.send(` Sua experiencia é -> ${xp}`);
+    }
+
     if (comando === "ajuda" || comando === "help") {
         message.channel.send(` Nossos comandos são: "!ping", "!dado" `)
     }
@@ -32,18 +93,17 @@ client.on("message", async message => {
             let valor = message.content.substr(5);
             let rolagem = valor.split('d');
             let total;
-            if(rolagem.length == 2){
+            if (rolagem.length == 2) {
                 let count = 0;
                 let valorAntigo = 0;
-                while(count < rolagem[0]){
-                    
-                    total = valorAntigo + Math.floor(Math.random() * rolagem[1] ) + 1;
-                    valorAntigo = total; 
-                    count ++;
-                    console.log(valorAntigo);
+                while (count < rolagem[0]) {
+
+                    total = valorAntigo + Math.floor(Math.random() * rolagem[1]) + 1;
+                    valorAntigo = total;
+                    count++;
                 }
             }
-            else{
+            else {
                 total = Math.floor(Math.random() * valor) + 1;
             }
             if (!isNaN(total)) {
